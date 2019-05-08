4ec2064a48f18ae8dca59dd44c203b163faaac02
Jorie Fernandez <joriefefernandez@yahoo.com>
2019-05-08 09:53:17 -0700

    Scaffolded City Explorer

--- src/main.js
+++ src/main.js
@@ -1,2 +1,28 @@
 import React from 'react';
+import Search from './search.js';
+import Map from './map.js';
+import Result from './result.js';
+
+class Main extends React.Component{
+  constructor(props){
+    super(props);
+  }
+
+  render(){
+    return(
+      <React.Fragment>
+        <Search />
+        <Map />
+        <Result />
+        <Result />
+        <Result />
+        <Result />
+        <Result />
+      </React.Fragment>
+     
+    );
+  }
+}
+
+export default Main;
 
